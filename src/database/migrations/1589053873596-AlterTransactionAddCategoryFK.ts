6810b169abd8f2086eca8caec631d92106af228d
digaodev <rodrigo.rb.dev@gmail.com>
2020-05-09 16:55:00 -0300

    feat: add initial config, migrations and models.

---
+++ src/database/migrations/1589053873596-AlterTransactionAddCategoryFK.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class AlterTransactionAddCategoryFK1589053873596
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'TransactionCategoryFK',
+        columnNames: ['category_id'],
+        referencedTableName: 'categories',
+        referencedColumnNames: ['id'],
+        onDelete: 'SET NULL',
+        onUpdate: 'SET NULL',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'TransactionCategoryFK');
+  }
+}
